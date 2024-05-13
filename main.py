c0341213f0ac30dd5ef7bd3e3b7d44784bccdafa
Gift-py <giftabumere247@gmail.com>
2024-05-13 15:18:43 +0100

    cors fix

--- main.py
+++ main.py
@@ -5,9 +5,21 @@ from user.controller import router as user_router
 from message.controller import  router as message_router
 from office.controller import router as office_router
 from generate_reports.controller import router as report_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+origins = [
+    'http://localhost:5173',
+    '*'
+]
+
+app.add_middleware(CORSMiddleware,
+                   allow_origins=origins,
+                   allow_credentials=True,
+                   allow_methods=['*'],
+                   allow_headers=['*'])
 
 app.include_router(user_router, prefix='/user')
 app.include_router(message_router, prefix='/messages')
